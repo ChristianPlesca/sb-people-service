408ea03aecac81de41409a56eec1c1e3c69791da
Cristian Plesca <cplesca@zaizi.com>
2021-06-18 13:04:25 +0100

    Get people API

--- src/main/java/com/example/peopleservice/person/control/PersonService.java
+++ src/main/java/com/example/peopleservice/person/control/PersonService.java
@@ -1,6 +1,10 @@
 package com.example.peopleservice.person.control;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.example.peopleservice.person.boundary.PersonRepository;
+import com.example.peopleservice.person.entity.Person;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,4 +17,14 @@ public class PersonService {
     
     @Autowired
     PersonRepository personRepository;
+
+    public List<Person> getPeople() {
+        
+        Iterable<Person> people = personRepository.findAll();
+
+        List<Person> peopleList = new ArrayList<Person>();
+        people.forEach(peopleList::add);
+
+        return peopleList;
+    }
 }

a485d1a334ba62696fbdfbb4ed379657da516ae4
Cristian Plesca <cplesca@zaizi.com>
2021-06-18 12:05:42 +0100

    Added validation methods to the Person fields

--- src/main/java/com/example/peopleservice/person/entity/Person.java
+++ src/main/java/com/example/peopleservice/person/entity/Person.java
@@ -1,9 +1,14 @@
 package com.example.peopleservice.person.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 import org.springframework.data.annotation.Id;
 
@@ -15,8 +20,16 @@ public class Person {
 
     @Id
     private Long id;
+
+    @NotBlank(message = "First name must not be blank")
     private String firstName;
+
+    @NotBlank(message = "Last name must not be blank")
     private String lastName;
+
+    @NotNull(message = "Date of birth must not be null")
     private LocalDate dateOfBirth;
+    
+    @NotBlank(message = "Country code must not be blank")
     private String countryCode;
 }
